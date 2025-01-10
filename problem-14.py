71c7e91213dbd8d60fcfcbe1dbd5343beea537bb
deblina <deblina@uchicago.edu>
2025-01-10 11:07:25 -0500

    adding memoized soltn

--- problem-14.py
+++ problem-14.py
@@ -25,3 +25,26 @@ for i in range(1, 1000000):
         max = collatz_len(i)
         greatest_starting_num = i
 print(greatest_starting_num)
+
+
+
+# can memoize to speed things up a big 
+def collatz_len(starting_num: int, memo: dict) -> int:
+    """
+    Takes in a potential starting number and a memoization dictionary.
+    Returns the length of the Collatz sequence.
+    """
+    if starting_num in memo:
+        return memo[starting_num]
+    
+    if starting_num == 1:
+        return 1
+    
+    if starting_num % 2 == 0:
+        next_num = starting_num // 2
+    else:
+        next_num = 3 * starting_num + 1
+    
+    length = 1 + collatz_len(next_num, memo)
+    memo[starting_num] = length
+    return length    
