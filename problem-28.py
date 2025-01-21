87dba20f9c2e0aa4b97750765f3d85334b0f0800
deblina <deblina@uchicago.edu>
2025-01-20 23:15:12 -0500

    meh

--- problem-28.py
+++ problem-28.py
@@ -7,3 +7,17 @@ p28 = lambda n=1001: 2*(n**3 - n)//3 + (n**2 - 1)//2 + 2*n - 1
 print(p28())
 
 
+# okay back with some clarifications 
+# the corners follow this pattern: 
+    # top-right: n^2 
+    # top-left: n^2 - (n - 1)
+    # bottom-left: n^2 - 2(n - 1)
+    # bottom-right: n^2 - 3(n - 1)
+# so the sum of corners is 4n^2 - 6(n - 1)
+def diff_p28(size:int)->int:
+    total = 1 # this is the base case, center 
+    for n in range(3, size + 1, 2): # only odd layers 
+        total += 4 * n ** 2 - 6 * (n - 1)
+    return total  
+
+print(diff_p28(1001))
