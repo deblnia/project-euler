22780665beabbd79755677f4f9efddd61f372ccd
Deblina Mukherjee <mynameisdeb.m@gmail.com>
2025-01-30 17:59:13 -0500

    actually logged this time, changed email

--- problem-40.py
+++ problem-40.py
@@ -4,3 +4,15 @@ from functools import reduce
 # creds to https://www.thehiddenblade.com/project-euler-now-all-problems-50-solved
 s=''.join([str(i) for i in range(1,1_000_000)])
 print(reduce(lambda x,y: x*y, map(int,[s[0],s[9],s[99],s[999],s[9999],s[99999],s[999999]])))
+
+
+s2 = '' 
+for i in range(1, 1_000_000): 
+	s2 += str(i) 
+
+acc = 1 
+for i in range(1, 1_000_000): 
+	if i == 0 or i == 9 or i == 99 or i == 999 or i == 9999 or i == 99_999 or i == 999_999: 
+		print(i) 
+		acc *= int(s2[i]) 
+print(acc)  
